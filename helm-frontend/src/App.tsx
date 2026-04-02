1c1eae9c37ec26374fcda5f768012e7d50377674
Harvey Punsalan <punsalanharvey019@gmail.com>
2026-04-02 17:14:31 +0800

    project scaffolding with React, Vite, TypeScript, and Tailwind

---
+++ helm-frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 flex items-center justify-center">
+      <h1 className="text-white text-3xl font-bold">Helm is alive</h1>
+
+    </div>
+  )
+}
+
+export default App
